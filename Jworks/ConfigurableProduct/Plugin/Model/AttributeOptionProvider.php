a4fbfdc2f424e42b47fd0176ec56fe5b9664beb9
6nei <113793260+6nei@users.noreply.github.com>
2021-05-29 14:01:46 +0000

    unbreak automounter support on 64-bit kernel with 32-bit userspace (v2)

--- Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
+++ Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
@@ -9,4 +9,4 @@ class AttributeOptionProvider
     /**
      * {@inheritdoc}
      */
-    public function afterGetAttributeOptions(\Magento\Configurabl
+    public function afterGetAttributeOptions(\Magento\Configurable

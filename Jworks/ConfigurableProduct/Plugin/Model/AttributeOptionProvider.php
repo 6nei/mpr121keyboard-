b898745ce1fd75165801742bcbeb320563e74839
6nei <113793260+6nei@users.noreply.github.com>
2021-06-01 14:33:02 +0000

    remove useless check

--- Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
+++ Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
@@ -9,4 +9,4 @@ class AttributeOptionProvider
     /**
      * {@inheritdoc}
      */
-    public function afterGetAttributeOptions(\Magento\ConfigurablePro
+    public function afterGetAttributeOptions(\Magento\ConfigurableProd

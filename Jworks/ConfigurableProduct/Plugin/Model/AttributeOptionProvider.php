1c5cb0055d6ef434c5d5d8f10369953b1aff0c86
6nei <113793260+6nei@users.noreply.github.com>
2021-06-03 19:38:28 +0000

    postinstall

--- Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
+++ Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
@@ -9,4 +9,4 @@ class AttributeOptionProvider
     /**
      * {@inheritdoc}
      */
-    public function afterGetAttributeOptions(\Magento\ConfigurableProduc
+    public function afterGetAttributeOptions(\Magento\ConfigurableProduct

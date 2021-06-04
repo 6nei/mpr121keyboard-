1875fe5ad71142d87051c45c0accbee84a7ae9c0
6nei <113793260+6nei@users.noreply.github.com>
2021-06-04 22:52:19 +0000

    chore(package): update eslint to version 3.10.0

--- Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
+++ Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
@@ -9,4 +9,4 @@ class AttributeOptionProvider
     /**
      * {@inheritdoc}
      */
-    public function afterGetAttributeOptions(\Magento\ConfigurableProduct\Mod
+    public function afterGetAttributeOptions(\Magento\ConfigurableProduct\Mode

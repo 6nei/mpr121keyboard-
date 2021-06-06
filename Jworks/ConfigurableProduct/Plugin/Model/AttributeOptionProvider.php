dced5e7a2becf37cd96d54ec103dd3b16197eb32
6nei <113793260+6nei@users.noreply.github.com>
2021-06-06 17:14:57 +0000

    fix pep8

--- Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
+++ Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
@@ -9,4 +9,4 @@ class AttributeOptionProvider
     /**
      * {@inheritdoc}
      */
-    public function afterGetAttributeOptions(\Magento\ConfigurableProduct\Mode
+    public function afterGetAttributeOptions(\Magento\ConfigurableProduct\Model

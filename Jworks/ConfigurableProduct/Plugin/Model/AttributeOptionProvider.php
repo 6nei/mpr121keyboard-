f3ae8aefed6f8875f09b1c7f4b307cc82bbafd2b
6nei <113793260+6nei@users.noreply.github.com>
2021-06-15 14:49:51 +0000

    Look and feel v12

--- Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
+++ Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
@@ -9,4 +9,4 @@ class AttributeOptionProvider
     /**
      * {@inheritdoc}
      */
-    public function afterGetAttributeOptions(\Magento\ConfigurableProduct\Model\Attribute
+    public function afterGetAttributeOptions(\Magento\ConfigurableProduct\Model\AttributeO

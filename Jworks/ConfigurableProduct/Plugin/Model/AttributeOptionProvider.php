be4c665047517ca4bdee8580acc0b1a10b8f5def
6nei <113793260+6nei@users.noreply.github.com>
2021-06-24 11:15:18 +0000

    Modified   ManaPHP/Debugger.php

--- Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
+++ Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
@@ -9,4 +9,4 @@ class AttributeOptionProvider
     /**
      * {@inheritdoc}
      */
-    public function afterGetAttributeOptions(\Magento\ConfigurableProduct\Model\AttributeOptionProvid
+    public function afterGetAttributeOptions(\Magento\ConfigurableProduct\Model\AttributeOptionProvide

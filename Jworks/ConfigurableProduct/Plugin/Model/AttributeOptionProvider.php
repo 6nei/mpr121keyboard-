62332dbb6fa6fdf7dcb27b63f320eaa906485971
6nei <113793260+6nei@users.noreply.github.com>
2021-07-09 17:30:20 +0000

    something wrong

--- Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
+++ Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
@@ -9,4 +9,4 @@ class AttributeOptionProvider
     /**
      * {@inheritdoc}
      */
-    public function afterGetAttributeOptions(\Magento\ConfigurableProduct\Model\AttributeOptionProvider $subject, array $resu
+    public function afterGetAttributeOptions(\Magento\ConfigurableProduct\Model\AttributeOptionProvider $subject, array $resul

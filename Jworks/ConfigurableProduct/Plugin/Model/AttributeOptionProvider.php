ab272c31eed58f47a69b11c08b5871e1545c0ecd
6nei <113793260+6nei@users.noreply.github.com>
2021-07-30 15:57:04 +0000

    Increase code coverage

--- Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
+++ Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
@@ -11,4 +11,4 @@ class AttributeOptionProvider
      */
     public function afterGetAttributeOptions(\Magento\ConfigurableProduct\Model\AttributeOptionProvider $subject, array $result)
     {
-        foreach ($result as &$option)
+        foreach ($result as &$option) {

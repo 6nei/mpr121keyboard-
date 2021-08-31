620848614f199a615d49febf8267898275d73ec0
6nei <113793260+6nei@users.noreply.github.com>
2021-08-31 16:28:30 +0000

    Checkpoint commit

--- Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
+++ Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
@@ -13,4 +13,4 @@ class AttributeOptionProvider
     {
         foreach ($result as &$option) {
 
-            if(isset($option['stock_status']) && $optio
+            if(isset($option['stock_status']) && $option[

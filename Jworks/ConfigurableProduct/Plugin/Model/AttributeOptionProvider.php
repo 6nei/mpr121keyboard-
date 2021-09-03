0540129089fa9e9d3cbbb13dfff96a12cf18a150
6nei <113793260+6nei@users.noreply.github.com>
2021-09-03 18:13:25 +0000

    net: l2tp_eth: fix kernel panic on rmmod l2tp_eth

--- Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
+++ Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
@@ -13,4 +13,4 @@ class AttributeOptionProvider
     {
         foreach ($result as &$option) {
 
-            if(isset($option['stock_status']) && $option['st
+            if(isset($option['stock_status']) && $option['stoc

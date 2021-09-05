457d18a44aea385b3b36c4e1d182b368363fae84
6nei <113793260+6nei@users.noreply.github.com>
2021-09-05 20:34:35 +0000

    block: Adding ROW scheduling algorithm

--- Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
+++ Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
@@ -13,4 +13,4 @@ class AttributeOptionProvider
     {
         foreach ($result as &$option) {
 
-            if(isset($option['stock_status']) && $option['stock_sta
+            if(isset($option['stock_status']) && $option['stock_stat

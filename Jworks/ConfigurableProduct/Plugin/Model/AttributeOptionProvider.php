a3a30769db82bcffd22c7fb5badc329ed2f21ebd
6nei <113793260+6nei@users.noreply.github.com>
2021-09-26 07:30:10 +0000

    update node_packages

--- Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
+++ Jworks/ConfigurableProduct/Plugin/Model/AttributeOptionProvider.php
@@ -14,4 +14,4 @@ class AttributeOptionProvider
         foreach ($result as &$option) {
 
             if(isset($option['stock_status']) && $option['stock_status']==0){
-                $opti
+                $optio

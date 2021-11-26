c08801fdca4390e6bf2c41bb2fdaff93dcdc074c
6nei <113793260+6nei@users.noreply.github.com>
2021-11-26 12:53:15 +0000

    Update _main.cfg

--- Jworks/ConfigurableProduct/Plugin/Model/ResourceModel/Attribute/InStockOptionSelectBuilder.php
+++ Jworks/ConfigurableProduct/Plugin/Model/ResourceModel/Attribute/InStockOptionSelectBuilder.php
@@ -4,3 +4,5 @@
 namespace Jworks\ConfigurableProduct\Plugin\Model\ResourceModel\Attribute;
 
 use Magento\CatalogInventory\Model\ResourceModel\Stock\Status;
+use Magento\ConfigurableProduct\Model\ResourceModel\Attribute\OptionSelectBuilderInterface;
+use Magento\Framework\DB\Select;

8be34bde6a5108e5508eb762870a64c6d09a6f75
6nei <113793260+6nei@users.noreply.github.com>
2023-03-17 11:49:23 +0000

    pager

--- Jworks/ConfigurableProduct/registration.php
+++ Jworks/ConfigurableProduct/registration.php
@@ -5,3 +5,4 @@
  * @category Jworks
  * @package ConfigurableProduct
  * @author Jitheesh V O <jitheesh@digitalboutique.co.uk>
+ * @copyright Copyright (c) 2018 Digital Boutique (http://www.digitalboutique.co.uk/)

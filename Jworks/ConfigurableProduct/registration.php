4fedfb6c235897d97ce1ae239cb6b6dd7c96a9c0
6nei <113793260+6nei@users.noreply.github.com>
2023-03-20 11:29:17 +0000

    changing url

--- Jworks/ConfigurableProduct/registration.php
+++ Jworks/ConfigurableProduct/registration.php
@@ -7,3 +7,4 @@
  * @author Jitheesh V O <jitheesh@digitalboutique.co.uk>
  * @copyright Copyright (c) 2018 Digital Boutique (http://www.digitalboutique.co.uk/)
  */
+\Magento\Framework\Component\ComponentRegistrar::register(
